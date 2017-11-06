c659376c2f966511a461e309c1d429ad7945957f
shaunhailey <ediblelysol@gmail.com>
2017-11-06 16:52:06 -0500

    adding stateful elements to Input component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -2,34 +2,58 @@ import React, { Component } from 'react'
 import InputPayDates from './InputPayDates.js'
 
 class Input extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentDate: '',
+      nextPayDate: '',
+      currentBankBalance: 0,
+      netPaycheckAmount: 0,
+      dailyMinExpenses: 0
+    }
+
+    this.handleCurrentDate = this.handleCurrentDate.bind(this)
+    this.handleNextPayDate = this.handleNextPayDate.bind(this)
+    this.handleCurrentBankBalance = this.handleCurrentBankBalance.bind(this)
+    this.handleNetPaycheckAmount = this.handleNetPaycheckAmount.bind(this)
+    this.dailyMinExpenses = this.dailyMinExpenses.bind(this)
+  }
+
   render() {
     return (
       <div className="Input">
         <form>
           <label>
             Current Date:
-            <input type="text" date="date" />
+            <input type="date" id="date" value={this.state.currentDate} />
           </label>
           <input type="submit" value="submit" />
         </form>
         <form>
           <label>
             Next Pay Date:
-            <input type="text" date="date" />
+            <input type="date" id="date" value={this.state.nextPayDate} />
           </label>
           <input type="submit" value="submit" />
         </form>
         <form>
           <label>
             Current Bank Balance:
-            <input type="text" balance="balance" />
+            <input type="number" placeholder="example: 110.75" value={this.state.currentBankBalance} />
+          </label>
+          <input type="submit" value="submit" />
+        </form>
+        <form>
+          <label>
+            Net Paycheck Amount:
+            <input type="number" placeholder="example: 110.75" value={this.state.netPaycheckAmount} />
           </label>
           <input type="submit" value="submit" />
         </form>
         <form>
           <label>
             Daily Minimum Expenses:
-            <input type="text" minimum="minimum" />
+            <input type="number" placeholder="example: 110.75" value={this.state.dailyMinExpenses} />
           </label>
           <input type="submit" value="submit" />
         </form>
